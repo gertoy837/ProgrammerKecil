8b8239a869494267b9524ec56b443ecdda5c2d67
mutiarhma15@gmail.com <mutiarhma15@gmail.com>
2026-04-09 13:29:11 +0700

    Create Admin Middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+
+const prisma = new PrismaClient({
+  errorFormat: 'minimal',
+});
 
 exports.register = async (req, res) => {
     res.json({ message: 'Register endpoint ready' });
